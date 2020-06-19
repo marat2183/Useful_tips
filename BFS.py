e019fb62e8ba33ec32c2b8ea03b71fc3c979c49a
Marat Grigoryan <marat.grig318@gmail.com>
2020-06-19 11:16:44 +0300

    BFS

---
+++ BFS.py
@@ -0,0 +1,15 @@
+def BFS(start, V, D):
+    D[start] = 0
+    Q = [start]
+    Qstart = 0
+    while Qstart < len(Q):
+        u = Q[Qstart]
+        print(u)
+        Qstart += 1
+        for v in V[u]:
+            if D[v] is None:
+                D[v] = D[u] + 1
+                Q.append(v)
+graph = [[4], [2, 4], [1, 3, 4], [2], [0, 1, 2]]
+D = [None] * len(graph)
+BFS(3, graph, D)
